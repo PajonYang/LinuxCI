e1dcec1b4c4583e1c5b76accf046744177d438ed
PajonYang[Wicresoft] <v-peiy@microsoft.com>
2018-05-16 14:57:31 +0800

    Update Add.c

--- Add.c
+++ Add.c
@@ -1,20 +1,10 @@
-
 #include <stdio.h>
-
 #include <stdlib.h>
-
 int main(int argc, char *argv[])
-
 {
-
 int x = atoi(argv[1]);
-
 int y = atoi(argv[2]);
-
 int result =  x + y;
-
 printf("%d", result);
-
 return 0;
-
 }
