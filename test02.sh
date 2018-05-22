2f6d9eb4d0c133915ea194897b27865254df2b2d
PajonYang[Wicresoft] <v-peiy@microsoft.com>
2018-05-22 18:07:49 +0800

    Update test02.sh

--- test02.sh
+++ test02.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 gcc -o add Add.c
-c= ./add 1 2
-if $c=4
-then
-        echo "Verify add 1+2=4, pass"
-else
-        echo "test fail"
-        exit -1
+c=`./add 1 2`
+
+if [ "$c" = "4" ];then
+        echo "test pass"
+else 
+        echo "error"
+        exit 1
 fi
